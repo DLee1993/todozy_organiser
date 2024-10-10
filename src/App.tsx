c2e52da5a4c08b40724be29f52b5cca790a8a6a8
David Lee <Dai180293@gmail.com>
2024-10-10 23:25:09 +0100

    added function to toggle theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,27 @@
 function App() {
-    return <>hello world</>;
+
+    function toggleTheme() {
+        const currentTheme = document.documentElement.getAttribute("data-theme");
+        const newTheme = currentTheme === "light" ? "dark" : "light";
+        document.documentElement.setAttribute("data-theme", newTheme);
+    }
+
+    return (
+        <>
+            <header>
+                <h1>Todozy</h1>
+                <button id="themeToggle" onClick={toggleTheme}></button>
+            </header>
+            <main>
+                input
+                <section id="todoList-container">
+                    <section id="todoList">list of todos</section>
+                    <section>filter buttons and list counter</section>
+                </section>
+                <span id="user-hint">Drag and drop to re-order the list</span>
+            </main>
+        </>
+    );
 }
 
 export default App;
