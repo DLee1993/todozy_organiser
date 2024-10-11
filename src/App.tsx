7b66c15de693c429f458da2cbc3bc2fec6f52fc7
David Lee <Dai180293@gmail.com>
2024-10-11 22:40:34 +0100

    added all crud and filter functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,120 @@
+import { FormEvent, useEffect, useState } from "react";
+
+type Todo = {
+    checked: boolean;
+    content: string;
+};
+
 function App() {
+    const [filter, setFilter] = useState("all");
+    const [todo, settodo] = useState("");
+    const [todoList, setTodoList] = useState<Todo[]>([]);
+    const allTodos = localStorage.getItem("todo-list");
+
+    useEffect(() => {
+        const refList = allTodos ? JSON.parse(allTodos) : [];
+
+        if (filter === "all" || filter === "") {
+            if (allTodos) {
+                setTodoList(JSON.parse(allTodos));
+            } else {
+                setTodoList([]);
+            }
+        }
+
+        if (filter === "active") {
+            const activeTodos = refList.filter((todo: Todo) => todo.checked === false);
+
+            if (activeTodos) {
+                setTodoList(activeTodos);
+            } else {
+                setTodoList([]);
+            }
+        }
+
+        if (filter === "completed") {
+            const completedTodos = refList.filter((todo: Todo) => todo.checked === true);
+
+            if (completedTodos) {
+                setTodoList(completedTodos);
+            } else {
+                setTodoList([]);
+            }
+        }
+    }, [filter, allTodos]);
+
+    const DeleteButton = ({ item }: { item: string }) => {
+        return (
+            <button id="deleteButton" onClick={() => deleteTodo(item)}>
+                <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+                    <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
+                    <g id="SVGRepo_iconCarrier">
+                        {" "}
+                        <path
+                            d="M4 7H20"
+                            stroke="#000000"
+                            strokeWidth="2"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                        ></path>{" "}
+                        <path
+                            d="M6 7V18C6 19.6569 7.34315 21 9 21H15C16.6569 21 18 19.6569 18 18V7"
+                            stroke="#000000"
+                            strokeWidth="2"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                        ></path>{" "}
+                        <path
+                            d="M9 5C9 3.89543 9.89543 3 11 3H13C14.1046 3 15 3.89543 15 5V7H9V5Z"
+                            stroke="#000000"
+                            strokeWidth="2"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                        ></path>{" "}
+                    </g>
+                </svg>
+            </button>
+        );
+    };
 
     function toggleTheme() {
         const currentTheme = document.documentElement.getAttribute("data-theme");
         const newTheme = currentTheme === "light" ? "dark" : "light";
         document.documentElement.setAttribute("data-theme", newTheme);
+    }
+
+    function addTodo(e: FormEvent) {
+        e.preventDefault();
+        settodo("");
+        const updatedState = [...todoList, { checked: false, content: todo }];
+
+        if (!updatedState) return;
+
+        setTodoList(updatedState);
+        localStorage.setItem("todo-list", JSON.stringify(updatedState));
+    }
+
+    function updateTodo(data: Todo) {
+        const updatedTodos = todoList.map((todo: Todo) =>
+            todo.content === data.content ? { ...todo, checked: data.checked } : todo
+        );
+
+        localStorage.setItem("todo-list", JSON.stringify(updatedTodos));
+
+        const updatedList = localStorage.getItem("todo-list");
+
+        if (updatedList) {
+            setTodoList(JSON.parse(updatedList));
+        } else {
+            setTodoList([]);
+        }
+    }
+
+    function deleteTodo(item: string) {
+        const updatedTodos = todoList.filter((todo: Todo) => todo.content !== item);
+        setTodoList(updatedTodos);
+        localStorage.setItem("todo-list", JSON.stringify(updatedTodos));
     }
 
     return (
@@ -13,12 +124,83 @@ function App() {
                 <button id="themeToggle" onClick={toggleTheme}></button>
             </header>
             <main>
-                input
+                <form onSubmit={addTodo}>
+                    <fieldset>
+                        <label htmlFor="todo">create a new todo</label>
+                        <input
+                            type="text"
+                            id="todo"
+                            placeholder="Create a new todo..."
+                            value={todo}
+                            onChange={(e) => settodo(e.target.value)}
+                            autoFocus
+                        />
+                    </fieldset>
+                </form>
                 <section id="todoList-container">
-                    <section id="todoList">list of todos</section>
-                    <section>filter buttons and list counter</section>
+                    <section id="todoList">
+                        <ul>
+                            {todoList.map((item: Todo, index: number) => (
+                                <li key={index}>
+                                    <span id="checkbox">
+                                        <input
+                                            id={`todo-${index}`}
+                                            readOnly
+                                            type="checkbox"
+                                            checked={item.checked}
+                                            onClick={() =>
+                                                updateTodo({
+                                                    checked: !item.checked,
+                                                    content: item.content,
+                                                })
+                                            }
+                                        />
+                                        <p>{item.content}</p>
+                                    </span>
+                                    <DeleteButton item={item.content} />
+                                </li>
+                            ))}
+                        </ul>
+                    </section>
+                    {todoList.length !== 0 && (
+                        <>
+                            <section id="filter">
+                                <p>
+                                    {todoList.length}{" "}
+                                    <span>{todoList.length > 1 ? "items" : "item"} left</span>
+                                </p>
+                                <ul>
+                                    <li>
+                                        <button
+                                            onClick={() => setFilter("all")}
+                                            className={filter === "all" ? "active" : ""}
+                                        >
+                                            All
+                                        </button>
+                                    </li>
+                                    <li>
+                                        <button
+                                            onClick={() => setFilter("active")}
+                                            className={filter === "active" ? "active" : ""}
+                                        >
+                                            Active
+                                        </button>
+                                    </li>
+                                    <li>
+                                        <button
+                                            onClick={() => setFilter("completed")}
+                                            className={filter === "completed" ? "active" : ""}
+                                        >
+                                            Completed
+                                        </button>
+                                    </li>
+                                </ul>
+                                <button>Clear completed</button>
+                            </section>
+                            <p id="user-hint">Drag and drop to re-order the list</p>
+                        </>
+                    )}
                 </section>
-                <span id="user-hint">Drag and drop to re-order the list</span>
             </main>
         </>
     );
